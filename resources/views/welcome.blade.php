1316fee3c7b2c091c9c1bc67ce00e18dc226b7a9
AchLim <rimuakoru@gmail.com>
2023-03-22 23:46:19 +0700

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CV - Vincent Lim</title>
+    <link href="https://fonts.googleapis.com/css?family=Mukta:300,400,500,600,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/vendors/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="resume.css">
+</head>
+
+<body>
+    <div class="content-wrapper">
+        <aside>
+            <div class="profile-img-wrapper">
+                <img src="#" alt="profile">
+            </div>
+            <h1 class="profile-name">Vincent Lim</h1>
+            <div class="text-center">
+                <span class="badge badge-white badge-pill profile-designation">Software Engineer</span>
+            </div>
+            <nav class="social-links">
+                <a href="https://www.instagram.com/linj.p_/" class="social-link"><i class="fab fa-instagram"></i></a>
+                <a href="https://github.com/AchLim" class="social-link"><i class="fab fa-github"></i></a>
+            </nav>
+            <div class="widget">
+                <h5 class="widget-title">Personal Information</h5>
+                <div class="widget-content">
+                    <p>BIRTHDAY : 13 July 2004</p>
+                    <p>MAIL : rimuakoru@gmail.com</p>
+                    <p>Location : Batam, Indonesia</p>
+                </div>
+            </div>
+            <div class="widget card">
+                <div class="card-body">
+                    <div class="widget-content">
+                        <h5 class="widget-title card-title">Languages</h5>
+                        <p>Indonesian : Native</p>
+                        <p>English : Fluent</p>
+                        <p>Hokkian : Fluent</p>
+                    </div>
+                </div>
+            </div>
+            <div class="widget card">
+                <div class="card-body">
+                    <div class="widget-content">
+                        <h5 class="widget-title card-title">INTERESTS</h5>
+                        <p>Arduino</p>
+                        <p>Programming</p>
+                        <p>Game Development</p>
+                    </div>
+                </div>
+            </div>
+        </aside>
+        <main>
+            <section class="resume-section">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h6 class="section-subtitle">RESUME</h6>
+                        <h2 class="section-title">EDUCATION</h2>
+                        <ul class="time-line">
+                            <li class="time-line-item">
+                                <span class="badge badge-primary">2022 - Current</span>
+                                <h6 class="time-line-item-title">Bachelor of Information Systems</h6>
+                                <p class="time-line-item-subtitle">University</p>
+                                <p class="time-line-item-content">
+                                    
+                                </p>
+                            </li>
+                            <li class="time-line-item">
+                                <span class="badge badge-primary">2019 - 2022</span>
+                                <h6 class="time-line-item-title">Holy Word Christian Highschool</h6>
+                                <p class="time-line-item-subtitle">Highschool</p>
+                                <p class="time-line-item-content">
+                                    Learned HTML, CSS, PHP, C / C++.
+                                </p>
+                            </li>
+                            <li class="time-line-item">
+                                <span class="badge badge-primary">2016 - 2019</span>
+                                <h6 class="time-line-item-title">Holy Word Christian School</h6>
+                                <p class="time-line-item-subtitle">Middle School</p>
+                                <p class="time-line-item-content">
+                                    Learned Photoshop and CorelDraw X6.
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <h6 class="section-subtitle">RESUME</h6>
+                        <h2 class="section-title">Experience</h2>
+                        <ul class="time-line">
+                            <li class="time-line-item">
+                                <span class="badge badge-primary">June 2022 - Current</span>
+                                <h6 class="time-line-item-title">Software Engineer</h6>
+                                <p class="time-line-item-subtitle">Software Development</p>
+                                <p class="time-line-item-content">
+                                    Has developed various ERP system such as HR Management, Training Management, Event Management.
+                                    Learned Python, Odoo Framework, PostgreSQL, Javascript, CSS, Linux Systems.
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+            <footer>
+                <a href="#" target="_blank" rel="noopener noreferrer">Vincent Lim</a> - 2023
+            </footer>
+        </main>
+    </div>
+</body>
+
+</html>
